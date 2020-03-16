baea45c272b21ee7856c2b8df6b242a3aa351d11
Hilary Ly <hilaryly@gmail.com>
2020-03-16 01:17:21 -0400

    files moved

---
+++ secrets.js
@@ -0,0 +1,11 @@
+process.env.CLARIFAI_API_KEY = 'Key a395be4a9c2646829e27a6bd618c0cb9';
+process.env.FIREBASE_CONFIG = {
+   apiKey: 'AIzaSyDTH1nFsvMne4FHu9GKZDS5odLqw_rmWj0',
+   //'AIzaSyDT18FFBHi5JYjpYCTLeB9x11GkGcU2qJQ',
+   authDomain: 'seefood-b071f.firebaseapp.com',
+   databaseURL: 'https://seefood-b071f.firebaseio.com',
+   storageBucket: 'seefood-b071f.appspot.com'
+};
+process.env.SPOONACULAR_API_KEY = 'a5dd96fbb6cd4fc298a30e4c580006f1';
+process.env.EDAMAM_API_KEY = 'ef694c897c1443c7b5d2565b5b2f6055';
+process.env.EDAMAM_APP_ID = 'd015fdde';
